3a22c09d4ecb5a6c3a547aca2eb2f06f3441a5ff
han_bao <han_bao@sunwave.com.xn>
2021-03-30 16:45:37 +0800

    pthread_mutex做部分修改

--- pthread/pthread_test.c
+++ pthread/pthread_test.c
@@ -8,29 +8,33 @@ pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 void *read_count()
 {
-    pthread_mutex_lock(&lock);
+    //pthread_mutex_lock(&lock);
     printf("count is %d\r\n",count);
-    pthread_mutex_unlock(&lock);
+    //pthread_mutex_unlock(&lock);
+    sleep(20);
 }
 
 void *plus_count()
 {
-    pthread_mutex_lock(&lock);
+    sleep(1);
+    //pthread_mutex_lock(&lock);
     count++;
-    pthread_mutex_unlock(&lock);
+    //pthread_mutex_unlock(&lock);
 }
 
 int main()
 {
-    pthread_t pthread_tid[100];
+    pthread_t pthread_tid[400];
     int i = 0;
-    for(i = 0;i<99;i++)
+    for(i = 0;i<400;i++)
     {
-        if(i%2)
-            pthread_create(&pthread_tid[i],NULL,(void *)plus_count,NULL);
-        else
-            pthread_create(&pthread_tid[i],NULL,(void *)read_count,NULL);
+        pthread_create(&pthread_tid[i],NULL,(void *)plus_count,NULL);
     }
-    sleep(2);
+    for(i = 0;i<400;i++)
+    {
+       pthread_join(pthread_tid[i],NULL);
+    }
+     printf("~~~~~~~~~~count is %d~~~~~~~~~~~~~~~\r\n",count);
     pthread_mutex_destroy(&lock);
+    return 0;
 }
